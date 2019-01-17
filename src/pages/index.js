e5317dfba818f5c8b6f4b57a6c3b77a0e23ada63
Edward Whelan <vroxing@gmail.com>
2019-01-17 11:02:26 -0500

    change extension for image files

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Layout from '../components/layout'
 import paritySite from '../images/parity.png'
 import rarecapsuleSite from '../images/rare-capsule.png'
 import umoodSite from '../images/u-mood.png'
-import bridge from '../images/bridgeMural.JPEG'
+import bridge from '../images/bridgeMural.jpeg'
 import naughtySite from '../images/naughtySite.png'
 
 const IndexPage = () => (
